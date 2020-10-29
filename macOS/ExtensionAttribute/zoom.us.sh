e47ce2db82ff3e74b8c2e99794d4dba9fb4d95c9
greatkemo <ktaynaz@gmail.com>
2020-10-29 15:52:43 +0300

    Update zoom.us.sh

--- macOS/ExtensionAttribute/zoom.us.sh
+++ macOS/ExtensionAttribute/zoom.us.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+if [[ -d /Applications/zoom.us.app ]]; then
+  zoom_version=$(defaults read "/Applications/zoom.us.app/Contents/info.plist" CFBundleVersion)
+  echo "<result>${zoom_version}</result>"
+else
+  echo "<result>Not Installed</result>"
+fi
+
+exit 0
